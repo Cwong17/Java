c5e5ac5810f85edeffab5880a2d416c57e1505b1
Chi Yuen Calvin Wong <cwong17@hawk.iit.edu>
2018-07-13 23:19:42 -0500

    Add files via upload

---
+++ MoreOperators.java
@@ -0,0 +1,15 @@
+public class MoreOperators {
+    public static void main(String[] args){
+        //Conversion by casting
+        int a = 2;
+        double b = 2.2;
+        char c = '1';
+
+        //conversion
+        double d = 2;           //2.0 instead 2
+        int e = (int)12.3;      //e = 12
+        double f = 2/3;         //f = 0.0
+        double g = (double)2/3; //g = 0.6666
+
+    }
+}
---
+++ SecondProgram.java
@@ -0,0 +1,6 @@
+public class SecondProgram {
+    public static void main(String[] args){
+        System.out.println("This is line 1.");
+        System.out.println("This is line 2.");
+    }
+}
